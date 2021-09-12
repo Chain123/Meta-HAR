d875421a19ef445364f722687142161add99f2e0
Chenglin Li <ch11@ualberta.ca>
2021-09-12 16:20:00 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -52,3 +52,48 @@ class heter_data(data.Dataset):
 
     def __len__(self):
         return len(self.data)
+
+    
+class heter_data2(data.Dataset):
+    def __init__(self, filename, transform=None, target_transform=None, target="hot"):
+        """ Input data with key values: "feature", "label" (one-hot), "user", 
+            'label_t' (local label).
+            Feature with shape [num_sampels, seqp_len, 8*2,  interval_len]
+        """
+        self.transform = transform
+        self.target_transform = target_transform
+        self.data = []
+        self.targets = []
+        self.targets_t = []
+
+        for file in filename:
+            data_tmp = utils.load_pickle(file)
+            self.data.extend(data_tmp["feature"])
+            if target == "hot":  # is one-hot required?
+                self.targets.extend(data_tmp["label"])
+            else:  # Train classifier, don't need one-hot encoding
+                self.targets.extend(np.argmax(data_tmp["label"], axis=1))
+            self.targets_t.extend(data_tmp["label_t"])
+
+        self.targets = np.array(self.targets)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target, target_t) 
+            target is index of the global target class.
+            target is index of the local target class.            
+        """
+        img, target, target_t = self.data[index], self.targets[index], self.targets_t[index]
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+            target_t = self.target_transform(target_t)
+
+        return img, target, target_t
+
+    def __len__(self):
+        return len(self.data)    
